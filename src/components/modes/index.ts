19dc2e706ed3bd7124613206f8a907f72c9b8b40
Alexander Rudenka <mur.mailbox@gmail.com>
2019-10-04 15:26:23 +0300

    Register mode

--- src/components/modes/index.ts
+++ src/components/modes/index.ts
@@ -3,3 +3,4 @@ export * from './build_road_mode';
 export * from './mark_district_mode';
 export * from './edit_district_mode';
 export * from './edit_crossroad_mode';
+export * from './buildings_editor_mode';
