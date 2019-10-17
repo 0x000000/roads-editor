39f362e8243b0a5e74c7ac6b5d60f912a48b6278
Alexander Rudenka <mur.mailbox@gmail.com>
2019-10-17 13:30:26 +0300

    Bump version

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
-export const BUILD_VERSION = 52;
+export const BUILD_VERSION = 53;
 export const POINT_DISTANCE = 20;
 export const FIELD_WIDTH = 30;
 export const FIELD_HEIGHT = 30;
 export const DOT_RADIUS = 2;
 export const CROSSROAD_DOT_RADIUS = DOT_RADIUS + 3;
 export const ROAD_WIDTH = 4;
+export const SLOT_SIZE = 28;
