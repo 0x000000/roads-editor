93415d040185ffbf5512223ad00fb4e5baaa868f
Alexander Rudenka <mur.mailbox@gmail.com>
2020-01-15 11:39:04 +0300

    Add more random utils

--- src/models/utils.ts
+++ src/models/utils.ts
@@ -1,3 +1,14 @@
+export interface GenerationChance<T> {
+  readonly min: number;
+  readonly max: number;
+  readonly value: T;
+}
+
+export interface Range {
+  min: number;
+  max: number;
+}
+
 /* tslint:disable */
 export interface CycledPair<T> {
   readonly start: T;
@@ -28,4 +39,52 @@ export function rand100() {
 
 export function inRange(value: number, min: number, max: number) {
   return value >= min && value <= max;
+}
+
+function parseFormula(formula: string): number[][] {
+  let prev = 0;
+
+  return formula.split(',').map(value => parseInt(value, 10)).map(value => {
+    const range = prev === 0 ? [prev, value + prev] : [prev + 1, value + prev];
+    prev += value;
+
+    return range;
+  });
+}
+
+function assertProbabilities(formula: string): boolean {
+  return formula.split(',')
+    .map(value => parseInt(value, 10))
+    .reduce((total, current) => total + current, 0) === 100;
+}
+
+export function calculateChances<T>(formula: string, ...params: T[]): Array<GenerationChance<T>> {
+  if (params.length !== formula.split(',').length) {
+    throw new Error('Wrong params count');
+  }
+
+  if (!assertProbabilities(formula)) {
+    throw new Error('Sum of chances != 100%');
+  }
+
+  return parseFormula(formula).map(([min, max], index) => {
+    return {min, max, value: params[index]};
+  });
+}
+
+export function selectByChance<T>(chances: Array<GenerationChance<T>>): T {
+  const dice = rand100();
+  return (chances.find(c => inRange(dice, c.min, c.max)) as GenerationChance<T>).value;
+}
+
+export function decide(range: Range): number {
+  return random(range.min, range.max);
+}
+
+export function r(min: number, max: number): Range {
+  return {min, max};
+}
+
+export function sample<T>(array: T[]): T {
+  return array[Math.floor(Math.random() * array.length)];
 }
